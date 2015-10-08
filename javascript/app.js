f98c7063abb2e2341f4385d1be2c083c935cb1f1
Nora Top <top.nora@gmail.com>
2015-10-08 23:25:41 +0000

    exercise-3 ready for review

--- javascript/app.js
+++ javascript/app.js
@@ -1,1 +1,13 @@
-// Write your code here!
+$('#signup-form').on('submit', function(event) {
+    var pwd = $("input[name=password]").val();
+    var pwd2 = $("input[name=password2]").val();
+    
+    if (pwd.length < 10) {
+        alert("Your password should have at least 10 characters");
+        event.preventDefault();
+    }
+    else if (pwd !== pwd2) {
+        alert("Your confirm password does not match your password");
+        event.preventDefault();
+    }
+});
