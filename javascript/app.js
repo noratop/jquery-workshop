0fb0ea1495f8cb21ab3554fc1aef9ba49267e5f5
Nora Top <top.nora@gmail.com>
2015-10-08 23:09:58 +0000

    exercise-2 ready for review

--- javascript/app.js
+++ javascript/app.js
@@ -1,1 +1,20 @@
-// Write your code here!
+var image = $("img");
+
+
+image.each(function(){
+    
+    var imgHTMLelt = '<img src="'+$(this).attr("src")+'" alt="'+$(this).attr("alt")+'">';
+    var altHTMLelt = '<p>'+$(this).attr("alt")+'</p>';
+    
+    
+    $(this).on("click",function(){
+        var popup = $('<div class = "overlay">'+imgHTMLelt+altHTMLelt+'</div>');
+        $("body").append(popup);
+        
+        $("div.overlay").on("click",function(){
+            //alert("fds");
+            $(this).remove();
+        });
+    });
+});
+
