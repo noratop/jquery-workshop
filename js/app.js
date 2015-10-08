0d4dfa29d86ec79cfc538913bd587a4169d53d1d
Nora Top <top.nora@gmail.com>
2015-10-08 22:03:01 +0000

    exercise-1

--- js/app.js
+++ js/app.js
@@ -1,6 +1,25 @@
 var ann = $("div.announcement");
 // alert("cc");
-ann.each(function(){
+ann.each(function() {
+    var current = $(this);
     var closeButton = $("<button>X</button>");
-    $(this).append(closeButton);
+    var showButton = $("<button>Show announcement</button>");
+    
+    current.append(closeButton);
+    current.append(showButton);
+    showButton.hide();
+    
+    closeButton.on("click", function close(){
+        
+        current.find("p").hide();
+        closeButton.hide();
+        showButton.show();
+    })
+    
+    showButton.on("click", function show(){
+        
+        current.find("p").show('slow');
+        showButton.hide();
+        closeButton.show();
+    })    
 });
